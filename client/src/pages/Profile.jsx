0c5292aac7df8c7176bdadbf6b07417998411796
Dimeji Rufai <rfdimeji@gmail.com>
2024-04-16 22:49:44 +0100

    update & re-adjustment

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,19 +8,20 @@ import {
 } from "firebase/storage"
 import { app } from "../firebase";
 import { 
-  updateUserStart, 
-  updateUserSuccess, 
-  updateUserFailure, 
-  deleteUserFailure, 
-  deleteUserSuccess, 
-  signOutUserStart
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 
 const Profile = () => {
-  const fileRef = useRef(null)
+  const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
@@ -30,131 +31,141 @@ const Profile = () => {
   const [showListingsError, setShowListingsError] = useState(false);
   const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
-  console.log(formData)
+
+  // firebase storage
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
   useEffect(() => {
-    if(file) {
+    if (file) {
       handleFileUpload(file);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [file]);
 
-const handleFileUpload = (file) => {
-  const storage = getStorage(app)
-  const fileName = new Date().getTime() + file.name
-  const storageRef = ref(storage, fileName)
-  const uploadTask = uploadBytesResumable(storageRef, file)
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-  uploadTask.on("state_changed",
-    (snapshot) => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      setFilePerc(Math.round(progress))
-    },
-    (error) => {
-      setFileUploadError(true)
-    },
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then(
-        (downloadURL) => 
-          setFormData({ ...formData, avatar: downloadURL })
-      )
-    }
-  );
-};
-
-const handleChange = (e) => {
-  setFormData({ ...formData, [e.target.id]: e.target.value })
-};
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    dispatch(updateUserStart())
-    const res = await fetch(`/api/user/update/${currentUser._id}`, {
-
-      method: "POST",
-      headers: {
-        'Content-Type': "application/json"
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
       },
-      body: JSON.stringify(formData)
-    })
-    const data = await res.json()
-    if (data.success === false) {
-      dispatch(updateUserFailure(data.message));
-      return;
-    }
-    dispatch(updateUserSuccess(data));
-    setUpdateSuccess(true)
-  } catch (error) {
-    dispatch(updateUserFailure(error.message));
-  }
-};
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
 
-const handleDeleteUser =async () => {
-  try {
-    dispatch(updateUserStart());
-    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
-      method: 'DELETE',
-    });
-    const data = await res.json();
-    if (data.success === false) {
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      // eslint-disable-next-line no-undef
       dispatch(deleteUserFailure(data.message));
-      return;
     }
-    dispatch(deleteUserSuccess(data));
-  } catch (error) {
-    dispatch(deleteUserFailure(error.message));
-  }
-};
+  };
 
-const handleSignOut = async () => {
-  try {
-    dispatch(signOutUserStart());
-    const res =await fetch(`/api/auth/signout`);
-    const data = await res.json();
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
 
-    if (data.success === false) {
-      dispatch(deleteUserFailure(data.message));
-      return;
-    }
-    dispatch(deleteUserSuccess(data));
-  } catch (error) {
-    dispatch(deleteUserFailure(error.message));
-  }
-}
-
-const handleShowListings = async () => {
-  try {
-    setShowListingsError(false)
-    const res = await fetch(`/api/user/listings/${currentUser._id}`);
-    const data = await res.json();
-    if (data.success === false) {
+      setUserListings(data);
+    } catch (error) {
       setShowListingsError(true);
-      return;
     }
-    setUserListings(data)
-  } catch (error) {
-    showListingsError(true);
-  }
-}
+  };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
 
-const handleListingDelete = async (listingId) => {
-  try {
-    const res = await fetch(`/api/listing/delete/${listingId}`, {
-      method: "DELETE",
-    });
-    const data = await res.json();
-    if (data.success === false) {
-      console.log(data.message);
-      return;
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
     }
-
-    setUserListings((prev) => prev.filter((listing) => listing._id !== listingId))
-  } catch (error) {
-    console.log(error.message)
-  }
-}
+  };
   return (
-    <div className="p-3 ma-w-lg mx-auto">
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>
         Profile
       </h1>
